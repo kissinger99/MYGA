a5914f95eacdf783e44973a772ce2191a9e781c8
Andrea <andreaprola@outlook.com>
2023-07-07 20:25:18 +0200

    I have adjusted and cleaned my lines following more your structure and logic. There only a small question in Population.py line 16

--- Population.py
+++ Population.py
@@ -1,28 +1,28 @@
+import random
+import string
+
 from Individual import Individual
 from statistics import mean
 
-population_size = 10
 class Population:
-    def __init__(self):
-        self.population = self.random_population()
-        self.average = self.fitness_average()
-    def random_population(self):
-        population = []
-        for i in range(population_size):
-            individual_random = Individual()
-            population.append(individual_random)
-        return population
-    def fitness_average(self):
+    def __init__(self, population_size: int, target: str):
+        self.population_size = population_size
+        self.target = target
+        self.individuals = []
+
+    def initialize_population(self):
+        for i in range(self.population_size):
+            genetics = self.random_genetic()
+            self.individuals.append(Individual(genetics, self.target)) #why self.target?
+    def random_genetic(self) -> str:
+        genetic_choices = random.choices(string.ascii_lowercase + " ", k=len(self.target))
+        genetics = "".join(genetic_choices)
+        return genetics
+    def print_population(self):
+        for individual in self.individuals:
+            print(individual)
+    def population_fitness_average(self):
         scores = []
-        for individual in self.population:
+        for individual in self.individuals:
             scores.append(individual.fitness)
         return mean(scores)
-
-    def __str__(self):
-        return "Population Fitness Average = " + str(self.average) + "\n" + \
-            str(self.population)
-    #why if I eliminate __repr__ in Individual I can't anymore see each Individual letters composition in the outcome?
-
-pop_one = Population()
-
-print(pop_one)
