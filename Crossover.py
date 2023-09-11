fac3f9a5deeccc389cab3977eb30c2da315381d4
Andrea <andreaprola@outlook.com>
2023-09-11 14:25:18 +0200

    I have created Selectio Methods: Roulette Wheel & Tournament

--- Crossover.py
+++ Crossover.py
@@ -3,7 +3,7 @@ from Individual import Individual
 from Population import Population
 
 crossover_probability = 0.98
-def crossover(parent_1: Individual, parent_2: Individual, population: Population, method: str):
+def crossover(parent_1: Individual, parent_2: Individual, method: str):
     prob = random.uniform(0,1)
 
     child_1 = parent_1
@@ -12,30 +12,32 @@ def crossover(parent_1: Individual, parent_2: Individual, population: Population
     if prob <= crossover_probability:
         match method:
             case "Single":
-                child_1, child_2 = one_point_crossover(parent_1, parent_2, population)
+                child_1, child_2 = one_point_crossover(parent_1, parent_2)
             case "Double":
-                child_1, child_2 = two_point_crossover(parent_1, parent_2, population)
+                child_1, child_2 = two_point_crossover(parent_1, parent_2)
     return child_1, child_2
 
-#ONE-POINT CROSSOVER
-def one_point_crossover(parent_1: Individual, parent_2: Individual, population: Population):
-    # select randomly one crossover point
-    cop = random.randint(0, len(parent_1.genetics) - 1)
-    #divide and swap
-    child_1 = parent_1.genetics[:cop] + parent_2.genetics[cop:]
-    child_2 = parent_2.genetics[:cop] + parent_1.genetics[cop:]
-
+def add_children_to_population(child_1: Individual, child_2: Individual, population: Population):
     population.individuals.append(child_1)
     population.individuals.append(child_2)
 
+#ONE-POINT CROSSOVER
+def one_point_crossover(parent_1: Individual, parent_2: Individual):
+    # select randomly one crossover point
+    cop = random.randint(0, len(parent_1.genetics) - 1)
+    #divide and swap
+    child_1_genetics = parent_1.genetics[:cop] + parent_2.genetics[cop:]
+    child_2_genetics = parent_2.genetics[:cop] + parent_1.genetics[cop:]
+
+    return Individual(genetics=child_1_genetics), Individual(genetics=child_2_genetics)
+
 #TWO-POINT CROSSOVER
-def two_point_crossover(parent_1: Individual, parent_2: Individual, population: Population):
+def two_point_crossover(parent_1: Individual, parent_2: Individual):
     #select randomly two crossover points
     cop1 = random.randint(1, len(parent_1.genetics)-1)
     cop2 = random.randint(cop1, len(parent_1.genetics)-1)
     #divide and swap
-    child_1 = parent_1.genetics[:cop1] + parent_2.genetics[cop1:cop2] + parent_1.genetics[cop2:]
-    child_2 = parent_2.genetics[:cop1] + parent_1.genetics[cop1:cop2] + parent_2.genetics[cop2:]
+    child_1_genetics = parent_1.genetics[:cop1] + parent_2.genetics[cop1:cop2] + parent_1.genetics[cop2:]
+    child_2_genetics = parent_2.genetics[:cop1] + parent_1.genetics[cop1:cop2] + parent_2.genetics[cop2:]
 
-    population.individuals.append(child_1)
-    population.individuals.append(child_2)
+    return Individual(genetics=child_1_genetics), Individual(genetics=child_2_genetics)
--- Population.py
+++ Population.py
@@ -7,12 +7,13 @@ class Population:
     def __init__(self, population_size: int, target: str):
         self.population_size = population_size
         self.target = target
+        Individual.set_target(self.target)
         self.individuals = []
 
     def initialize_population(self):
         for i in range(self.population_size):
             genetics = self.random_genetic()
-            self.individuals.append(Individual(genetics, self.target)) #why self.target?
+            self.individuals.append(Individual(genetics))
     def random_genetic(self) -> str:
         genetic_choices = random.choices(string.ascii_lowercase + " ", k=len(self.target))
         genetics = "".join(genetic_choices)
